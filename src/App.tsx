6cba113ffe1ac419b06de53cbccbf102edb2c94f
aljithkj02 <kwdeathlok@gmail.com>
2024-08-30 19:50:09 +0530

    Add basic react app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import '@/App.css'
+import { Button } from '@/components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <p className="text-3xl">Wallet</p>
+      <Button>Jithu</Button>
+    </>
+  )
+}
+
+export default App
