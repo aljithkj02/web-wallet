3b038d52fff2389930c91bce213fe3acde9a88a2
aljithkj02 <kwdeathlok@gmail.com>
2024-08-31 10:34:51 +0530

    Add import wallet feature & add toaster

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,47 @@ import { Navbar } from "@/components/common/Navbar"
 import { SeedPhrase } from '@/components/common/SeedPhrase'
 import { Accounts } from '@/components/common/Accounts'
 import { useState } from 'react'
+import { generateMnemonic } from 'bip39'
+import { Toaster } from '@/components/ui/toaster'
 
 function App() {
-  const [phrases, setPhrases] = useState<string[]>([
-      // "question", "question", "question", "question",
-      // "question", "question", "question", "question",
-      // "question", "question", "question", "question"
-  ])
+    const [mnemonic, setMnemonic] = useState("");
+    const [phrases, setPhrases] = useState<string[]>(new Array(12).fill(""))
 
-  const handleOnChanage = (index: number, value: string) => {
-    phrases[index] = value;
-    setPhrases([...phrases]);
-  }
+    const handleOnChanage = (index: number, value: string) => {
+        phrases[index] = value;
+        setPhrases([...phrases]);
+    }
 
-  return (
-    <div className='bg-[#09090b] min-h-screen select-none'>
-      <Navbar />
+    const createNewMnemonic = () => {
+        const newMnemonic = generateMnemonic();
+        setMnemonic(newMnemonic);
+        setPhrases(newMnemonic.split(' '));
+    }
 
-      <div className='pt-24 pb-10 px-10 md:px-40 flex flex-col gap-10'>
-        <SeedPhrase phrases={phrases} onChange={handleOnChanage} />
-        
-        <Accounts />
-      </div>
-    </div>
-  )
+    const handleImportWallet = (mnemonicText: string) => {
+        setMnemonic(mnemonicText);
+    }
+
+    return (
+        <div className='bg-[#09090b] min-h-screen select-none'>
+            <Navbar />
+
+            <div className='pt-24 pb-10 px-10 md:px-40 flex flex-col gap-10'>
+                <SeedPhrase 
+                    phrases={phrases} 
+                    onChange={handleOnChanage} 
+                    createNewMnemonic={createNewMnemonic}
+                    mnemonic={mnemonic}
+                    importWallet={handleImportWallet}
+                />
+                
+                <Accounts />
+            </div>
+
+            <Toaster />
+        </div>
+    )
 }
 
 export default App
--- src/components/common/SeedPhrase.tsx
+++ src/components/common/SeedPhrase.tsx
@@ -6,20 +6,38 @@ import {
   } from "@/components/ui/accordion"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input";
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
+import { useToast } from "@/hooks/use-toast"
 
 interface SeedPhraseProps {
     phrases: string[];
+    mnemonic: string;
     onChange: (index: number, value: string) => void;
+    createNewMnemonic: () => void;
+    importWallet: (text: string) => void;
 }
 
-export const SeedPhrase = ( { phrases, onChange }: SeedPhraseProps) => {
+export const SeedPhrase = ( { phrases, mnemonic, onChange, createNewMnemonic, importWallet }: SeedPhraseProps) => {
+    const [importSecret, setImportSecret] = useState(false);
+    const { toast } = useToast();
 
     const handleOnChanage = (e: ChangeEvent<HTMLInputElement>) => {
+        if (mnemonic) return;
+        
         const index = Number(e.target.name);
         const value = e.target.value;
 
         onChange(index, value);
+    }
+
+    const handleImportWallet = () => {
+        if (phrases.some((x) => x === '')) {
+            toast({description: "Fill all the fields!"});
+            return;
+        }
+        importWallet(phrases.join(' '));
+        toast({ description: "Wallet imported successfully!"});
+        setImportSecret(false);
     }
 
     return (
@@ -30,32 +48,58 @@ export const SeedPhrase = ( { phrases, onChange }: SeedPhraseProps) => {
                         Secret phrase
                     </AccordionTrigger>
 
-                    <AccordionContent className="px-6 py-6 text-white">
+                    <AccordionContent className="px-6 pt-4 pb-6 text-white">
                         <div className="flex justify-center items-center gap-6">
-                            { !phrases.length  && (
-                                <div><Button variant="secondary">Create a new secret recovery phrase?</Button></div>
+                            { (!mnemonic && !importSecret)  && (
+                                <div>
+                                    <Button variant="secondary"
+                                        onClick={createNewMnemonic}
+                                    >
+                                        Create a new secret recovery phrase?
+                                    </Button>
+                                </div>
                             )}
 
-                            { !phrases.length  && (
-                                <div><Button variant="secondary">Import secret recovery phrase?</Button></div> 
+                            { (!mnemonic && !importSecret)  && (
+                                <div>
+                                    <Button variant="secondary"
+                                        onClick={() => setImportSecret(true)}
+                                    >
+                                        Import secret recovery phrase?
+                                    </Button>
+                                </div> 
                             )}
                         </div>
 
                         <div className="grid grid-cols-4 mt-5">
                             {
-                                phrases.map((phrase, i) => {
+                                ( mnemonic || importSecret ) && phrases.map((phrase, i) => {
                                     return <div key={i}
                                         className="p-4 text-center text-[15px]"
                                     >
                                         <Input type="text"
                                             name={i.toString()}  
                                             value={phrase}
-                                            className="bg-black border-none outline-none"
+                                            className="bg-black border-none outline-none disabled:text-white"
                                             onChange={handleOnChanage}
+                                            autoComplete="off"
                                         />
                                     </div>
                                 })
                             }
+                        </div>
+
+                        <div>
+                            { importSecret && (
+                                <div className="flex mt-5 justify-center">
+                                    <Button variant="secondary"
+                                        className="w-60"
+                                        onClick={handleImportWallet}
+                                    >
+                                        Import Wallet
+                                    </Button>
+                                </div>
+                            )}
                         </div>
                     </AccordionContent>
                 </AccordionItem>
