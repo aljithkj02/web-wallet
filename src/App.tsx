6f81ecf47c46d66f7bbccf6e460f0bd94675b295
aljithkj02 <kwdeathlok@gmail.com>
2024-08-31 09:57:02 +0530

    Add Input elements in the seed phrase

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,26 @@ import '@/App.css'
 import { Navbar } from "@/components/common/Navbar"
 import { SeedPhrase } from '@/components/common/SeedPhrase'
 import { Accounts } from '@/components/common/Accounts'
+import { useState } from 'react'
 
 function App() {
+  const [phrases, setPhrases] = useState<string[]>([
+      // "question", "question", "question", "question",
+      // "question", "question", "question", "question",
+      // "question", "question", "question", "question"
+  ])
+
+  const handleOnChanage = (index: number, value: string) => {
+    phrases[index] = value;
+    setPhrases([...phrases]);
+  }
 
   return (
     <div className='bg-[#09090b] min-h-screen select-none'>
       <Navbar />
 
       <div className='pt-24 pb-10 px-10 md:px-40 flex flex-col gap-10'>
-        <SeedPhrase />
+        <SeedPhrase phrases={phrases} onChange={handleOnChanage} />
         
         <Accounts />
       </div>
--- src/components/common/SeedPhrase.tsx
+++ src/components/common/SeedPhrase.tsx
@@ -5,26 +5,40 @@ import {
     AccordionTrigger,
   } from "@/components/ui/accordion"
 import { Button } from "@/components/ui/button"
-import { useState } from "react"
-  
-export const SeedPhrase = () => {
-    const [phrases, setPhrases] = useState([
-        "question", "question", "question", "question",
-        "question", "question", "question", "question",
-        "question", "question", "question", "question"
-    ])
+import { Input } from "@/components/ui/input";
+import { ChangeEvent } from "react";
+
+interface SeedPhraseProps {
+    phrases: string[];
+    onChange: (index: number, value: string) => void;
+}
+
+export const SeedPhrase = ( { phrases, onChange }: SeedPhraseProps) => {
+
+    const handleOnChanage = (e: ChangeEvent<HTMLInputElement>) => {
+        const index = Number(e.target.name);
+        const value = e.target.value;
+
+        onChange(index, value);
+    }
 
     return (
         <div>
             <Accordion type="single" collapsible>
                 <AccordionItem value="item-1">
                     <AccordionTrigger className="px-6 text-white">
-                        Seed phrase
+                        Secret phrase
                     </AccordionTrigger>
 
-                    <AccordionContent className="px-6 py-10 text-white">
-                        <div className="flex justify-center">
-                            <Button variant="secondary">Create a new Seed phrase?</Button>
+                    <AccordionContent className="px-6 py-6 text-white">
+                        <div className="flex justify-center items-center gap-6">
+                            { !phrases.length  && (
+                                <div><Button variant="secondary">Create a new secret recovery phrase?</Button></div>
+                            )}
+
+                            { !phrases.length  && (
+                                <div><Button variant="secondary">Import secret recovery phrase?</Button></div> 
+                            )}
                         </div>
 
                         <div className="grid grid-cols-4 mt-5">
@@ -33,7 +47,12 @@ export const SeedPhrase = () => {
                                     return <div key={i}
                                         className="p-4 text-center text-[15px]"
                                     >
-                                        {phrase}
+                                        <Input type="text"
+                                            name={i.toString()}  
+                                            value={phrase}
+                                            className="bg-black border-none outline-none"
+                                            onChange={handleOnChanage}
+                                        />
                                     </div>
                                 })
                             }
