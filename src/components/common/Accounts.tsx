a4f047d22cf1024e56c20b10585b523969d24ac0
aljithkj02 <kwdeathlok@gmail.com>
2024-08-31 11:48:24 +0530

    Add show private key feature

--- src/components/common/Accounts.tsx
+++ src/components/common/Accounts.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { SolanaIcon, EthereumIcon } from '@/assets'
 import { IAccount } from "@/App";
 
@@ -11,6 +11,7 @@ interface AccoutnsProps {
 }
 
 export const Accounts = ( { mnemonic, index, accounts, generateNewAccount }: AccoutnsProps) => {
+    const [showKey, setShowKey] = useState("");
 
     useEffect(() => {
         if (mnemonic && index === 0) {
@@ -22,7 +23,7 @@ export const Accounts = ( { mnemonic, index, accounts, generateNewAccount }: Acc
         <div>
             <div className="flex items-center justify-between">
                 <p className='text-white text-xl'>My Accounts</p>
-                
+
                 <Button variant="secondary"
                     onClick={generateNewAccount}
                 >
@@ -32,10 +33,10 @@ export const Accounts = ( { mnemonic, index, accounts, generateNewAccount }: Acc
 
             <div className="my-5 flex flex-col gap-10">
                 {
-                    accounts.map((account, index) => {
+                    accounts.map((account, i) => {
                         return (
                             <div key={account.ethPublicKey} >
-                                <p className="text-white text-center mb-1 text-lg">Account {index + 1}</p>
+                                <p className="text-white text-center mb-1 text-lg">Account {i + 1}</p>
 
                                 <div className="border border-blue-500 px-4 py-2 rounded-lg">
                                     <div className="flex items-center gap-10 border-b border-slate-400 py-4 px-4 rounded-md">
@@ -53,7 +54,16 @@ export const Accounts = ( { mnemonic, index, accounts, generateNewAccount }: Acc
                                             
                                             <div className="flex items-center">
                                                 <p className="text-gray-400 w-[15%]">Private Key</p>
-                                                <p className="text-white w-[85%] text-sm">{ account.solPrivateKey }</p>
+                                                { showKey === `sol-${i}` ? (
+                                                    <p className="text-white w-[85%] text-sm">{ account.solPrivateKey }</p>
+                                                ) : (
+                                                    <Button
+                                                        className="w-[85%]"
+                                                        onClick={() => setShowKey(`sol-${i}`)}
+                                                    >
+                                                        Show Private Key
+                                                    </Button> 
+                                                )}
                                             </div>
                                         </div>
                                     </div>
@@ -73,7 +83,16 @@ export const Accounts = ( { mnemonic, index, accounts, generateNewAccount }: Acc
                                             
                                             <div className="flex items-center">
                                                 <p className="text-gray-400 w-[15%]">Private Key</p>
-                                                <p className="text-white w-[85%] text-sm">{ account.ethPrivateKey }</p>
+                                                { showKey === `eth-${i}` ? (
+                                                    <p className="text-white w-[85%] text-sm">{ account.ethPrivateKey }</p>
+                                                ) : (
+                                                    <Button
+                                                        className="w-[85%]"
+                                                        onClick={() => setShowKey(`eth-${i}`)}
+                                                    >
+                                                        Show Private Key
+                                                    </Button> 
+                                                )}
                                             </div>
                                         </div>
                                     </div>
